f3a7d2ee0c0778dd36376cdd8d5b6c4c7a193459
icanprogramming <icanprogramming@gmail.com>
2023-06-12 19:10:36 +0900

    사칙연산

---
+++ Baekjoon/10869.cpp
@@ -0,0 +1,13 @@
+// 사칙연산
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A = 0, B = 0;
+	cin >> A >> B;
+
+	cout << A + B << '\n' << A - B << '\n' << A * B << '\n' << A / B << '\n' << A % B;
+}

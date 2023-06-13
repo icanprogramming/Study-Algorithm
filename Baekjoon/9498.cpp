02ffa3ff3b88e63c104d0cbf37a66fa88c3be223
icanprogramming <icanprogramming@gmail.com>
2023-06-13 22:20:23 +0900

    시험 성적

---
+++ Baekjoon/9498.cpp
@@ -0,0 +1,32 @@
+// 시험 성적
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int testScore = 0;
+	cin >> testScore;
+
+	if (90 <= testScore)
+	{
+		cout << 'A';
+	}
+	else if (80 <= testScore)
+	{
+		cout << 'B';
+	}
+	else if (70 <= testScore)
+	{
+		cout << 'C';
+	}
+	else if (60 <= testScore)
+	{
+		cout << 'D';
+	}
+	else
+	{
+		cout << 'F';
+	}
+}

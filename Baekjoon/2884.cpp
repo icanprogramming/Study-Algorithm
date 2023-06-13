853450a7290b93d674caf949cb8bcccdb81b3e3d
icanprogramming <icanprogramming@gmail.com>
2023-06-13 22:57:39 +0900

    알람 시계

---
+++ Baekjoon/2884.cpp
@@ -0,0 +1,25 @@
+// 알람 시계
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int H = 0, M = 0;
+	cin >> H >> M;
+
+	int alarmH = H, alarmM = M - 45;
+	if (alarmM < 0)
+	{
+		alarmH -= 1;
+		alarmM += 60;
+
+		if (alarmH < 0)
+		{
+			alarmH += 24;
+		}
+	}
+
+	cout << alarmH << ' ' << alarmM;
+}

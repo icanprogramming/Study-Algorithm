b63ca5e9ed2529f6ca2eb1fac3fe6559c69d8787
icanprogramming <icanprogramming@gmail.com>
2023-06-16 22:47:48 +0900

    11654 : 아스키 코드

---
+++ Baekjoon/11654：아스키 코드.cpp
@@ -0,0 +1,18 @@
+// 11654번 : 아스키 코드
+// 주소 : https://www.acmicpc.net/problem/11654
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	char character = '\0';
+	cin >> character;
+
+	cout << (int)character;
+}

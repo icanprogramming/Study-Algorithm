4c61ac7d987fa827f320a1f5fc996ffe09007341
icanprogramming <icanprogramming@gmail.com>
2023-06-19 16:43:50 +0900

    4344 : 평균은 넘겠지

---
+++ Baekjoon/4344：평균은 넘겠지.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int C = 0;
+	cin >> C;
+
+	for (int i = 0; i < C; i++)
+	{
+		int numberOfStudent = 0;
+		cin >> numberOfStudent;
+
+		int studentScore[1'000] = { 0 };
+		int totalScore = 0;
+		for (int j = 0; j < numberOfStudent; j++)
+		{
+			int score = 0;
+			cin >> score;
+
+			studentScore[j] = score;
+			totalScore += score;
+		}
+
+		int average = totalScore / numberOfStudent;
+		int goodStudentCount = 0;
+		for (int j = 0; j < numberOfStudent; j++)
+		{
+			if (average < studentScore[j])
+			{
+				goodStudentCount++;
+			}
+		}
+
+		double goodStudentPercent = goodStudentCount / (double)numberOfStudent * 100;
+		goodStudentPercent = round(goodStudentPercent * 1'000) / 1'000;
+
+		cout << fixed;
+		cout.precision(3);
+		cout << goodStudentPercent << "%\n";
+	}
+}

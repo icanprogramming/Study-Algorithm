96340e030e2ade316712af9f1c47284839106898
icanprogramming <icanprogramming@gmail.com>
2023-06-23 21:42:23 +0900

    15894 : 수학은 체육과목 입니다

---
+++ Baekjoon/15894：수학은 체육과목 입니다.cpp
@@ -0,0 +1,14 @@
+// 15894번 : 수학은 체육과목 입니다
+// 주소 : https://www.acmicpc.net/problem/15894
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+
+	cout << n * 4;
+}
